0ed52dc2fd072e6a720e8e1713f47a335ffd4b5a
Vladislav <vstefanov@rnd-solutions.net>
2019-12-20 17:21:19 +0200

    Adding project with dockerfiles and travis CI configuration file.

---
+++ MyTestApp.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace MyTestApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
