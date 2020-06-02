4404a26f0f171784190e581eda48eae7599f244e
Vladislav <vstefanov@rnd-solutions.net>
2020-06-02 11:20:20 +0300

    Exposed ports of the Web and Api projects and added Redis Cache and RDS instance to the infrastructure.

--- Infrastructure/app-stack.tf
+++ Infrastructure/app-stack.tf
@@ -49,16 +49,85 @@ module "eks" {
             name = "${local.worker_group_name}-A"
             instance_type= "t2.small"
             asg_desired_capacity = 1
-            additional_security_group_ids = [aws_security_group.worker_group_A.id] 
+            additional_security_group_ids = [aws_security_group.worker_group_A.id, aws_security_group.postgres_redis.id] 
         },
         {
             name = "${local.worker_group_name}-B"
             instance_type="t2.small"
             asg_desired_capacity = 1
-            additional_security_group_ids = [aws_security_group.worker_group_B.id]
+            additional_security_group_ids = [aws_security_group.worker_group_B.id, aws_security_group.postgres_redis.id]
         }
     ]
 }
+
+resource "aws_db_instance" "postgres_db" {
+  allocated_storage    = 10
+  storage_type         = "standard"
+  engine               = "postgres"
+  engine_version       = "12.2"
+  instance_class       = "db.t2.micro"
+  name                 = "test-database"
+  username             = "postgres"
+  password             = "postgres_password_1234"
+  parameter_group_name = "default.postgres12.2"
+  db_subnet_group_name = aws_db_subnet_group.postgres_db.name
+  port                 = 5432
+  backup_retention_period = 3
+  vpc_security_group_ids = [aws_security_group.postgres_redis.id]
+}
+
+resource "aws_db_subnet_group" "postgres_db" {
+  name       = "postgres-db-subnet-group"
+  subnet_ids = ["${module.vpc.private_subnets[2]}"]
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-cluster"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis_cache.name
+  security_group_ids   =[aws_security_group.postgres_redis.id]
+}
+
+resource "aws_elasticache_subnet_group" "redis_cache" {
+  name       = "redis-cache-subnet-group"
+  subnet_ids =["${module.vpc.private_subnets[2]}"] 
+}
+
+resource "aws_security_group" "postgres_redis" {
+  name="postgres-redis-sg"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 6379
+    to_port   = 6379
+    protocol  = "tcp"
+    self = true
+  }
+
+    ingress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    self = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   tags = {
+    "Name" = "${local.worker_group_name}-B-sg"
+   }
+}
+
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
--- Infrastructure/infra-stack.tf
+++ Infrastructure/infra-stack.tf
@@ -10,8 +10,8 @@ module "vpc" {
   azs = [data.aws_availability_zones.available_zones.names[0],
          data.aws_availability_zones.available_zones.names[1]]
 
-  private_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
-  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24"]
+  private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24","10.10.103.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
